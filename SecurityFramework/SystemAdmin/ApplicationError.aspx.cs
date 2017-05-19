99a4945bcd50391d76f61d9f3a6c59541bd5762e
rdonalson <rick.donalson@comcast.net>
2017-05-19 15:26:09 -0400

    Updated DB Script

--- SecurityFramework/SystemAdmin/ApplicationError.aspx.cs
+++ SecurityFramework/SystemAdmin/ApplicationError.aspx.cs
@@ -5,40 +5,39 @@ namespace SecurityFramework.SystemAdmin
 {
     public partial class ApplicationsError : Page
     {
-        private Exception _ex;
-
         protected void Page_Load(object sender, EventArgs e)
         {
             try
             {
                 // Get the last error from the server
-                _ex = ApplicationCommon.LastException;
+                Exception ex = ApplicationCommon.LastException;
 
                 // Create a safe message
                 var safeMsg = "A problem has occurred in the web site. ";
 
                 // Show Inner Exception fields for local access
-                if (_ex?.InnerException != null)
+                if (ex?.InnerException != null)
                 {
-                    innerTrace.Text = _ex.InnerException.StackTrace;
+                    innerTrace.Text = ex.InnerException.StackTrace;
                     InnerErrorPanel.Visible = Request.IsLocal;
-                    innerMessage.Text = _ex.InnerException.Message;
+                    innerMessage.Text = ex.InnerException.Message;
                 }
                 // Show Trace for local access
                 if (Request.IsLocal)
                     exTrace.Visible = true;
                 else
-                    _ex = new Exception(safeMsg, _ex);
+                    ex = new Exception(safeMsg, ex);
 
                 // Fill the page fields
-                if (_ex != null)
+                if (ex != null)
                 {
-                    exMessage.Text = _ex.Message;
-                    exTrace.Text = _ex.StackTrace;
+                    exMessage.Text = ex.Message;
+                    exTrace.Text = ex.StackTrace;
                 }
 
                 // Clear the error from the server
                 Server.ClearError();
+                ApplicationCommon.LastException = null;
             }
             catch (Exception ex)
             {
