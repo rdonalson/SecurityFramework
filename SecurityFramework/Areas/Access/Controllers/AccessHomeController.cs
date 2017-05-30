05fa53593e65f2b5c35a798e49e28ccaf342f988
rdonalson <rick.donalson@comcast.net>
2017-05-30 14:16:33 -0400

    Minor Updates

--- SecurityFramework/Areas/Access/Controllers/AccessHomeController.cs
+++ SecurityFramework/Areas/Access/Controllers/AccessHomeController.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using SecurityFramework.Areas.Access.Models.Entity;
-using SecurityFramework.Areas.Access.Models.View;
+﻿using System.Web.Mvc;
 using SecurityFramework.Utilities.Common;
-using Role = SecurityFramework.Areas.Access.Models.View.Role;
-using Route = SecurityFramework.Areas.Access.Models.View.Route;
 
 namespace SecurityFramework.Areas.Access.Controllers
 {
