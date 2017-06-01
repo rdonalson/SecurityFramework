58c79d9b3b9c7b0bbc36f629891c989d4fe202cd
rdonalson <rick.donalson@comcast.net>
2017-06-01 13:54:25 -0400

    Setpoint: Got filter built for Objects within User's View

--- SecurityFramework/Areas/Access/Models/Utils/AccessUtils.cs
+++ SecurityFramework/Areas/Access/Models/Utils/AccessUtils.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using SecurityFramework.Areas.Access.Models.Entity;
 using SecurityFramework.Areas.Access.Models.View;
+using SecurityFramework.Utilities.Common;
 
 namespace SecurityFramework.Areas.Access.Models.Utils
 {
@@ -12,10 +15,14 @@ namespace SecurityFramework.Areas.Access.Models.Utils
     public class AccessUtils
     {
         private readonly AccessEntities _db;
+        private readonly List<Guid> _guids;
 
         public AccessUtils(AccessEntities db)
         {
             _db = db;
+            var appid = AppCommon.AppAttributeValue;
+            var userid = AppCommon.UserProfile.Id.ToString();
+            _guids = _db.spUserAreas(appid, userid).Select(l => l.Id).ToList();
         }
 
         #region Areas
@@ -23,6 +30,7 @@ namespace SecurityFramework.Areas.Access.Models.Utils
         public IQueryable<ListViewModel> GetDomains()
         {
             var domains = from dom in _db.Domains
+                where _guids.Contains(dom.Id)
                 orderby dom.Name
                 select new ListViewModel
                 {
@@ -37,10 +45,12 @@ namespace SecurityFramework.Areas.Access.Models.Utils
         {
             var organizations = from dom in _db.Domains
                 join org in _db.Organizations on dom.Id equals org.DomainId
+                where _guids.Contains(org.Id)
                 orderby dom.Name, org.Name
                 select new ListViewModel
                 {
-                    Breadcrumb = dom.Name + " > " + org.Name,
+                    Breadcrumb = dom.Name + " > "
+                                 + org.Name,
                     Id = org.Id,
                     Name = org.Name
                 };
@@ -52,6 +62,7 @@ namespace SecurityFramework.Areas.Access.Models.Utils
             var groups = from dom in _db.Domains
                 join org in _db.Organizations on dom.Id equals org.DomainId
                 join grp in _db.Groups on org.Id equals grp.OrganizationId
+                where _guids.Contains(grp.Id)
                 orderby dom.Name, org.Name, grp.Name
                 select new ListViewModel
                 {
@@ -68,10 +79,14 @@ namespace SecurityFramework.Areas.Access.Models.Utils
                 join org in _db.Organizations on dom.Id equals org.DomainId
                 join grp in _db.Groups on org.Id equals grp.OrganizationId
                 join off in _db.Offices on grp.Id equals off.GroupId
+                where _guids.Contains(off.Id)
                 orderby dom.Name, org.Name, grp.Name, off.Name
                 select new ListViewModel
                 {
-                    Breadcrumb = dom.Name + " > " + org.Name + " > " + grp.Name + " > " + off.Name,
+                    Breadcrumb = dom.Name + " > "
+                                 + org.Name + " > "
+                                 + grp.Name + " > "
+                                 + off.Name,
                     Id = off.Id,
                     Name = off.Name
                 };
@@ -85,10 +100,15 @@ namespace SecurityFramework.Areas.Access.Models.Utils
                 join grp in _db.Groups on org.Id equals grp.OrganizationId
                 join off in _db.Offices on grp.Id equals off.GroupId
                 join shp in _db.Shops on off.Id equals shp.OfficeId
+                where _guids.Contains(shp.Id)
                 orderby dom.Name, org.Name, grp.Name, off.Name, shp.Name
                 select new ListViewModel
                 {
-                    Breadcrumb = dom.Name + " > " + org.Name + " > " + grp.Name + " > " + off.Name + " > " + shp.Name,
+                    Breadcrumb = dom.Name + " > "
+                                 + org.Name + " > "
+                                 + grp.Name + " > "
+                                 + off.Name + " > "
+                                 + shp.Name,
                     Id = shp.Id,
                     Name = shp.Name
                 };
@@ -104,6 +124,7 @@ namespace SecurityFramework.Areas.Access.Models.Utils
             var domainRoles = from dom in _db.Domains
                 join rle in _db.Roles on dom.Id equals rle.DomainId
                 where rle.DomainId != null
+                      && _guids.Contains(dom.Id)
                 orderby dom.Name, rle.Sequence
                 select new ListViewModel
                 {
@@ -121,10 +142,12 @@ namespace SecurityFramework.Areas.Access.Models.Utils
                 join org in _db.Organizations on dom.Id equals org.DomainId
                 join rle in _db.Roles on org.Id equals rle.OrganizationId
                 where rle.OrganizationId != null
+                      && _guids.Contains(org.Id)
                 orderby dom.Name, org.Name, rle.Sequence
                 select new ListViewModel
                 {
-                    Breadcrumb = dom.Name + " > " + org.Name,
+                    Breadcrumb = dom.Name + " > "
+                                 + org.Name,
                     Id = rle.Id,
                     Name = rle.Name,
                     Sequence = rle.Sequence
@@ -139,10 +162,13 @@ namespace SecurityFramework.Areas.Access.Models.Utils
                 join grp in _db.Groups on org.Id equals grp.OrganizationId
                 join rle in _db.Roles on grp.Id equals rle.GroupId
                 where rle.GroupId != null
+                      && _guids.Contains(grp.Id)
                 orderby dom.Name, org.Name, grp.Name, rle.Sequence
                 select new ListViewModel
                 {
-                    Breadcrumb = dom.Name + " > " + org.Name + " > " + grp.Name,
+                    Breadcrumb = dom.Name + " > "
+                                 + org.Name + " > "
+                                 + grp.Name,
                     Id = rle.Id,
                     Name = rle.Name,
                     Sequence = rle.Sequence
@@ -158,10 +184,14 @@ namespace SecurityFramework.Areas.Access.Models.Utils
                 join off in _db.Offices on grp.Id equals off.GroupId
                 join rle in _db.Roles on off.Id equals rle.OfficeId
                 where rle.OfficeId != null
+                      && _guids.Contains(off.Id)
                 orderby dom.Name, org.Name, grp.Name, off.Name, rle.Sequence
                 select new ListViewModel
                 {
-                    Breadcrumb = dom.Name + " > " + org.Name + " > " + grp.Name + " > " + off.Name,
+                    Breadcrumb = dom.Name + " > "
+                                 + org.Name + " > "
+                                 + grp.Name + " > "
+                                 + off.Name,
                     Id = rle.Id,
                     Name = rle.Name,
                     Sequence = rle.Sequence
@@ -178,6 +208,7 @@ namespace SecurityFramework.Areas.Access.Models.Utils
                 join shp in _db.Shops on off.Id equals shp.OfficeId
                 join rle in _db.Roles on shp.Id equals rle.ShopId
                 where rle.ShopId != null
+                      && _guids.Contains(shp.Id)
                 orderby dom.Name, org.Name, grp.Name, off.Name, shp.Name, rle.Sequence
                 select new ListViewModel
                 {
